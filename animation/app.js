6817a6f42e62f0fd1848c70a8d8c545d63c07e36
Jordan <iraborjordan1@gmail.com>
2020-05-31 17:14:23 +0100

    included animation and transitions

--- animation/app.js
+++ animation/app.js
@@ -7,7 +7,7 @@ function animateSlides() {
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
 
-  sliders.forEach((slide) => {
+  sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -20,7 +20,59 @@ function animateSlides() {
     sliderT1.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     sliderT1.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
     sliderT1.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(sliderT1)
+      .addTo(controller);
+
+    const pageT1 = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    if(nextSlide === 'end') return false 
+    pageT1.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageT1.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageT1.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageT1)
+      .addTo(controller);
   });
 }
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
+
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e) {
+  const item = e.target;
+  console.log(e.target)
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("explore")) {
+      console.log('c')
+    mouse.classList.add("explore-active");
+    mouseTxt.innerText = "Tap";
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
+  }
+}
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 
 animateSlides();
